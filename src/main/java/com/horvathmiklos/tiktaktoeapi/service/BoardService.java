4b8144cb82028837d579c4a70cc57cfb518712df
HorvathMiklos <horvathmikloswork@gmail.com>
2022-07-08 23:29:08 +0200

    add BoardService

---
+++ src/main/java/com/horvathmiklos/tiktaktoeapi/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.horvathmiklos.tiktaktoeapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BoardService {
+    public char[][] calculateNextMove(char nextPlayerMark, char[][] currentBoard) {
+        return null;
+    }
+}
